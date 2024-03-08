bf428202cbf774701f76522a89246d99c0484b3b
J-K. Solbakken <jksolbakken@gmail.com>
2024-03-08 12:22:08 +0100

    typescriptify the whole shabang

---
+++ src/validator.ts
@@ -0,0 +1,108 @@
+'use strict'
+
+type LENGTH_ERROR = 'fnr, dnr or hnr must consist of 11 digits';
+type CHECKSUM_ERROR = "checksums don't match";
+type DATE_ERROR = 'invalid date';
+type ErrorReason = LENGTH_ERROR | CHECKSUM_ERROR | DATE_ERROR;
+type OkResult = { status: 'valid'; type: NrType };
+type NrType = 'dnr' | 'fnr' | 'hnr' | 'tnr' | 'dnr-and-hnr'
+type ErrorResult = { status: 'invalid'; reasons: ErrorReason[] };
+type ValidationResult = OkResult | ErrorResult;
+
+const elevenDigits = new RegExp('^\\d{11}$')
+
+export const fnr = (digits: string): ValidationResult => {
+   return idnr(digits)
+}
+
+export const dnr = (digits: string): ValidationResult => {
+   return idnr(digits)
+}
+
+export const hnr = (digits: string): ValidationResult => {
+   return idnr(digits)
+}
+
+export const tnr = (digits: string): ValidationResult => {
+   return idnr(digits)
+}
+
+export const dnrAndHnr = (digits: string): ValidationResult => {
+   return idnr(digits)
+}
+
+export const getType = (digits: string): NrType => {
+   let nrs = digits.split("").map(Number)
+   if (nrs[0] >= 4 && nrs[2] >= 4) {
+      return 'dnr-and-hnr'
+   }
+   else if (nrs[0] >= 4) {
+      return 'dnr'
+   } else if (nrs[2] >= 8) {
+      return "tnr"
+   } else if (nrs[2] >= 4) {
+      return 'hnr'
+   }
+   return 'fnr'
+}
+
+export const idnr = (digits: string): ValidationResult => {
+   const type = getType(digits)
+   return validate(digits, type)
+}
+
+const validate = (digits: string, type: NrType): ValidationResult => {
+   if (!elevenDigits.test(digits)) {
+      return {
+         status: "invalid",
+         reasons: ["fnr, dnr or hnr must consist of 11 digits"]
+      }
+   }
+
+   const errMsgs = [...checksums(digits), ...birthdate(digits, type)]
+
+   return errMsgs.length == 0 ? {status: "valid", type} :
+      {
+         status: "invalid",
+         reasons: errMsgs
+      }
+}
+
+const checksums = (digits: string): Array<CHECKSUM_ERROR> => {
+   let nrs = digits.split("").map(Number)
+   let k1 = 11 - ((3 * nrs[0] + 7 * nrs[1] + 6 * nrs[2] + 1 * nrs[3] + 8 * nrs[4] +
+      9 * nrs[5] + 4 * nrs[6] + 5 * nrs[7] + 2 * nrs[8]) % 11)
+   let k2 = 11 - ((5 * nrs[0] + 4 * nrs[1] + 3 * nrs[2] + 2 * nrs[3] + 7 *
+      nrs[4] + 6 * nrs[5] + 5 * nrs[6] + 4 * nrs[7] + 3 * nrs[8] + 2 * k1) % 11)
+
+   if (k1 === 11) k1 = 0
+   if (k2 === 11) k2 = 0
+
+   return k1 < 10 && k2 < 10 && k1 == nrs[9] && k2 == nrs[10] ?
+      [] : ["checksums don't match"]
+}
+
+// copied from https://stackoverflow.com/questions/5812220/how-to-validate-a-date
+const birthdate = (digits: string, type: string): Array<DATE_ERROR> => {
+   if (type === 'dnr') {
+      digits = (Number(digits.substring(0, 1)) - 4) + digits.substring(1)
+   } else if (type === 'hnr') {
+      digits = digits.substring(0, 2) + (Number(digits.substring(2, 3)) - 4) + digits.substring(3)
+   } else if (type === 'tnr') {
+      digits = digits.substring(0, 2) + (Number(digits.substring(2, 3)) - 8) + digits.substring(3)
+   } else if (type === 'dnr-and-hnr') {
+      digits = (Number(digits.substring(0, 1)) - 4) + digits.substring(1, 2) + (Number(digits.substring(2, 3)) - 4) + digits.substring(3)
+   }
+
+   const day = Number(digits.substring(0, 2))
+   const month = Number(digits.substring(2, 4))
+   const yearString = digits.substring(4, 6)
+   const year = yearString == '00' ? 2000 : Number(yearString)
+
+   // set year 00 default to 2000 instead of 1900
+   const date = new Date(year, month - 1, day)
+
+   return (date && (date.getMonth() + 1) == month && date.getDate() == day) ?
+      [] : ["invalid date"]
+ }
+
--- tests/fnr.spec.ts
+++ tests/fnr.spec.ts
@@ -1,14 +1,12 @@
 'use strict'
 
-import { fnr, dnr, hnr, tnr, dnrAndHnr } from '../src/validator.js'
-
-import { expect } from 'chai'
+import { fnr, dnr, hnr, tnr, dnrAndHnr } from '../src/validator'
 
 describe("fnr", function () {
 
    it("should accept a valid one", function () {
       const result = fnr("13097248022")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "fnr"
       })
@@ -16,7 +14,7 @@ describe("fnr", function () {
 
    it("should accept a standard leap year", function () {
       const result = fnr("29029648784")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "fnr"
       })
@@ -24,7 +22,7 @@ describe("fnr", function () {
 
    it("should accept year 00 as valid leap year", function () {
       const result = fnr("29020075838")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "fnr"
       })
@@ -32,7 +30,7 @@ describe("fnr", function () {
 
    it("should reject if date is > 28 feb in a non leap year", function () {
       const result = fnr("29020112345")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["checksums don't match", "invalid date"]
       })
@@ -40,7 +38,7 @@ describe("fnr", function () {
 
    it("should compensate for checksum digits that are 11", function () {
       const result = fnr("15021951940")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "fnr"
       })
@@ -48,7 +46,7 @@ describe("fnr", function () {
 
    it("should reject if less than 11 digits", function () {
       const result = fnr("1234567890")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["fnr, dnr or hnr must consist of 11 digits"]
       })
@@ -56,7 +54,7 @@ describe("fnr", function () {
 
    it("should reject if more than 11 digits", function () {
       const result = fnr("123456789101")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["fnr, dnr or hnr must consist of 11 digits"]
       })
@@ -64,7 +62,7 @@ describe("fnr", function () {
 
    it("should reject if non-digits are present", function () {
       const result = fnr("1234567891A")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["fnr, dnr or hnr must consist of 11 digits"]
       })
@@ -72,7 +70,7 @@ describe("fnr", function () {
 
    it("should reject if checksum 1 is invalid", function () {
       const result = fnr("13097248032")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["checksums don't match"]
       })
@@ -80,7 +78,7 @@ describe("fnr", function () {
 
    it("should reject if checksum 2 is invalid", function () {
       const result = fnr("13097248023")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["checksums don't match"]
       })
@@ -88,7 +86,7 @@ describe("fnr", function () {
 
    it("should reject if day is invalid", function () {
       const result = fnr("32127248022")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["checksums don't match", "invalid date"]
       })
@@ -96,7 +94,7 @@ describe("fnr", function () {
 
    it("should reject if month is invalid", function () {
       const result = fnr("13137248022")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "invalid",
          reasons: ["checksums don't match", "invalid date"]
       })
@@ -108,7 +106,7 @@ describe("dnr", function () {
    // dnr is identical to fnr except for the first digit
    it("should accept a valid one", function () {
       const result = dnr("53097248016")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "dnr"
       })
@@ -119,7 +117,7 @@ describe("hnr", function () {
    // hnr is identical to fnr except for the third digit
    it("should accept a valid one", function () {
       const result = hnr("13527248013")
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "hnr"
       })
@@ -129,7 +127,7 @@ describe("tnr", function () {
    // tnr is identical to fnr except for the third digit which is increased with 8
    it("should accept a valid one", function () {
       const result = tnr("10915596784");
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "tnr",
       });
@@ -141,7 +139,7 @@ describe("dnr-and-hnr", function () {
    // combined dnr and hnr - so both first and third digit is increased with 4
    it("should accept a valid one", function () {
       const result = dnrAndHnr("68467038838");
-      return expect(result).to.deep.equal({
+      return expect(result).toEqual({
          status: "valid",
          type: "dnr-and-hnr",
       });
