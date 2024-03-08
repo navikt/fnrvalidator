dcacd9baa422d179aba18947ddba32f3f031e87f
Andreas Nordahl <andnorda@gmail.com>
2024-03-08 12:31:03 +0100

    fix cjs/esm issue

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+module.exports = {
+   preset: "ts-jest",
+   testEnvironment: "node",
 };
